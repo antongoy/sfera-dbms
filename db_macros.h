fca24d7f06529823be0000c1197b8f72bdffbf48
Anton Goy <g.a.s.s@mail.ru>
2014-11-12 02:54:48 +0500

    Resize DB_HEADER for cache_size

--- db_macros.h
+++ db_macros.h
@@ -3,7 +3,7 @@
 
 #define MAX_SIZE_KEY 30
 #define MAX_SIZE_VALUE 30
-#define DB_HEADER_SIZE sizeof(size_t) * 4
+#define DB_HEADER_SIZE sizeof(size_t) * 5
 #define BYTE_SIZE 8
 #define OFFSET_SIZE 4
 #define MIN(a,b) ((a)>(b)?(b):(a))
