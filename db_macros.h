6af99584b11a408d7de1d3fd5d64d087fb3b08a0
antongoy <g.a.s.s@mail.ru>
2014-11-29 16:19:25 +0300

    Extend size of key and value

--- db_macros.h
+++ db_macros.h
@@ -1,8 +1,8 @@
 #ifndef _DB_MACROS_H_
 #define _DB_MACROS_H_
 
-#define MAX_SIZE_KEY 30
-#define MAX_SIZE_VALUE 30
+#define MAX_SIZE_KEY 33
+#define MAX_SIZE_VALUE 33
 #define DB_HEADER_SIZE sizeof(size_t) * 5
 #define BYTE_SIZE 8
 #define OFFSET_SIZE 4
